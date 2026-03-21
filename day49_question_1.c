cd71099df839061fdfbc52e7eff311c6165d6b74
ujjwal29-iu <ujjwaltyagi458@gmail.com>
2026-03-21 19:18:01 +0530

    DONE

---
+++ day49_question_1.c
@@ -0,0 +1,54 @@
+// Problem: BST Insert
+
+// Implement the solution for this problem.
+
+// Input:
+// - Input specifications
+
+// Output:
+// - Output specifications
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) return createNode(data);
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    int n, x;
+    struct Node* root = NULL;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+    inorder(root);
+    return 0;
+}
