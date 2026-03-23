33e61ff7bc05751b8987070888115f60b7e379dd
ujjwal29-iu <ujjwaltyagi458@gmail.com>
2026-03-23 12:57:26 +0530

    DONE

---
+++ day51_question_1.c
@@ -0,0 +1,69 @@
+// Problem Statement:
+// Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Search Tree.
+
+// Input Format:
+// - First line contains integer N
+// - Second line contains N space-separated integers
+// - Third line contains two node values
+
+// Output Format:
+// - Print the LCA value
+
+// Example:
+// Input:
+// 7
+// 6 2 8 0 4 7 9
+// 2 8
+
+// Output:
+// 6
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int val;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL) return newNode(val);
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+    return root;
+}
+
+int findLCA(struct Node* root, int p, int q) {
+    while (root) {
+        if (p < root->val && q < root->val)
+            root = root->left;
+        else if (p > root->val && q > root->val)
+            root = root->right;
+        else
+            return root->val;
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int val, p, q;
+    struct Node* root = NULL;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+    scanf("%d %d", &p, &q);
+    printf("%d", findLCA(root, p, q));
+    return 0;
+}
