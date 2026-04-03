25507799e8d0a39f796b8a2cebe42c40cf12f1e1
ujjwal29-iu <ujjwaltyagi458@gmail.com>
2026-04-03 10:11:17 +0530

    DONE

---
+++ day62_question_1.c
@@ -0,0 +1,67 @@
+// Problem: Build a graph with adjacency list representation. Use linked lists or dynamic arrays.
+
+// Input:
+// - n (vertices)
+// - m (edges)
+// - edges (u, v)
+
+// Output:
+// - List of adjacency lists for each vertex
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(struct Node* adj[], int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+void printGraph(struct Node* adj[], int n) {
+    for (int i = 0; i < n; i++) {
+        struct Node* temp = adj[i];
+        printf("%d:", i);
+        while (temp) {
+            printf(" %d", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    struct Node* adj[n];
+
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(adj, u, v);
+    }
+
+    printGraph(adj, n);
+
+    return 0;
+}
